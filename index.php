52bdff051c21dd949e7248584cdb07a536985ba5
Ginanjar Dwi Prasetyo <83767762+ginanjar-janabadra@users.noreply.github.com>
2023-01-13 18:21:41 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
- <!DOCTYPE html>    
- <html>    
- <head>  
- <title>Situs dalam Pemeliharaan</title>  
- </head>  
- <body>  
- <div style="text-align:center;margin-top:200px;">  
- <h1>Maaf situs sedang dalam tahap pembuatan.<br/>  
- Silahkan berkunjung lagi besok.</h1>  
- </div>  
- </body>    
- </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect to New Web</title>
+</head>
+<body>
+    
+</body>
+</html>
